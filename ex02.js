5de072b597a19da271c8306c3aca9e9207e3721e
Phtessarolo <phtessarolo@gmail.com>
2024-04-23 22:52:32 -0300

    Exercicio 02

---
+++ ex02.js
@@ -0,0 +1,11 @@
+// 2. Escreva um condicional que verifica se saldoConta é negativo. 
+// Se for verdadeiro, exiba "Saldo negativo", caso contrário, 
+// exiba "Saldo positivo".
+
+let saldoConta = -1;
+
+if (saldoConta > 0) {
+    console.log('Saldo Positivo');
+} else {
+    console.log('Saldo Negativo');
+}
