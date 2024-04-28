9178e706550d11349c6f07e295bde95658718a5d
Phtessarolo <phtessarolo@gmail.com>
2024-04-28 16:43:56 -0300

    ex09

---
+++ ex08.js
@@ -0,0 +1,9 @@
+// 8. Crie uma função chamada menorNumero que recebe três números como
+//  parâmetros e retorna o menor deles.
+
+function menorNumero(a, b, c) {
+
+    return Math.min(a, b, c)
+}
+
+console.log(menorNumero(1, 2, 3));
